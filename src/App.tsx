e5960a304199c19b99a0c070e4f3f36f16a7abe1
FrancescoEsk <francifrison.ff@gmail.com>
2025-04-28 11:35:45 +0200

    Aggiornato il layout e gli stili CSS per migliorare la presentazione dei componenti, inclusa la rimozione di bordi e l'aggiunta di classi per una migliore responsività

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import {
     SleepStageType,
 } from "./scripts/extractSleepStages.ts";
 
-
 function App() {
     const COLORS = ["#FF8042", "lightskyblue", "royalblue", "blue"];
     const [testo] = useState("Eccellente");
@@ -73,19 +72,23 @@ function App() {
     }, [loading]); // semplice stampa dei dati caricati ---- da togliere
 
     return (
-        <div>
+        <div className="bg-dark-subtle">
             {/* nel caso ci sia stato un errore nel caricare i dati, fa vedere l'errore */}
             {error && <p style={{ color: "red" }}>{error}</p>}
-            <div className="container">
-                <div className="riga1 row">
-                    <div className="carta card justify-content-center align-items-center text-center">
+            <div className="container-fluid mb-1 fixed-top">
+                <div className="row">
+                    <div className="card bg-dark-subtle justify-content-center align-items-center border-0 border-bottom border-1 border-light rounded-0">
                         <Card>
                             <Navbar></Navbar>
                         </Card>
                     </div>
                 </div>
+            </div>
+            {/* Spacer per evitare sovrapposizione */}
+            <div className="pt-5 mt-4"></div>
+            <div className="container-lg">
                 <div className="riga2 justify-content-center align-items-center row">
-                    <div className="carta card  text-center">
+                    <div className="card text-center border-4 rounded-4">
                         <Card>
                             <Punteggio
                                 punteggio={percentuale}
@@ -94,37 +97,48 @@ function App() {
                         </Card>
                     </div>
                 </div>
+            </div>
+            <div className="container-lg">
                 <div className="riga3 row">
-                    <div className="card col-6">
-                        <div className="mezzaCarta  text-center">
-                            <Card>
-                                <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "100%" }}>
-                                    <h1 className="title align-middle">HAI DORMITO</h1>
-                                    <div className="row text-center  align-middle">
-                                        <div className="dormito col-12">
-                                            <div className="grande mx-1 text-center">8</div>
-                                            <div className="text-center">h</div>
-                                            <div className="grande mx-1 text-center">20</div>
-                                            <div className="text-center">min</div>
+                    <div className="card col-12 col-md-6 border-4 rounded-4">
+                        <Card>
+                            <div
+                                className="d-flex flex-column justify-content-center align-items-center"
+                                style={{ height: "100%" }}
+                            >
+                                <h1 className="title align-middle">
+                                    HAI DORMITO
+                                </h1>
+                                <div className="row text-center  align-middle">
+                                    <div className="dormito col-12">
+                                        <div className="grande mx-1 text-center">
+                                            8
                                         </div>
+                                        <div className="text-center">h</div>
+                                        <div className="grande mx-1 text-center">
+                                            20
+                                        </div>
+                                        <div className="text-center">min</div>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
+                            </div>
+                        </Card>
                     </div>
-                    <div className="card col-6">
-                        <div className="mezzaCarta text-center justify-content-center align-items-center">
+                    <div className="card col-12 col-md-6 border-4 rounded-4">
+                        <div className="text-center justify-content-center align-items-center ">
                             <Card>
                                 <PieGraph
                                     data={sleepStages ? sleepStages : []}
-                                    colors={COLORS} />
+                                    colors={COLORS}
+                                />
                             </Card>
                         </div>
                     </div>
                 </div>
-
-                <div className="riga4  justify-content-center align-items-center row">
-                    <div className="carta big card ">
+            </div>
+            <div className="container-lg">
+                <div className="riga4 justify-content-center align-items-center row">
+                    <div className="big card border-4 rounded-4">
                         <Card>
                             <CreateScatterPlot
                                 dati={sleepData ? sleepData : []}
@@ -133,22 +147,28 @@ function App() {
                         </Card>
                     </div>
                 </div>
+            </div>
+            <div className="container-lg">
                 <div className="riga2 justify-content-center align-items-center row">
-                    <div className="carta card ">
+                    <div className="big card border-4 rounded-4">
                         <Card>
                             <Consiglio />
                         </Card>
                     </div>
                 </div>
+            </div>
+            <div className="container-lg">
                 <div className="riga4 justify-content-center align-items-center row">
-                    <div className="carta big card">
+                    <div className="big card border-4 rounded-4">
                         <Card>
                             <h1>SEGNAPOSTO X GRAFICO</h1>
                         </Card>
                     </div>
                 </div>
+            </div>
+            <div className="container-lg">
                 <div className="riga4 justify-content-center align-items-center row">
-                    <div className="carta big card">
+                    <div className="big card border-4 rounded-4">
                         <Card>
                             <h1>SEGNAPOSTO X INFORMAZIONI SUL SONNO</h1>
                         </Card>
